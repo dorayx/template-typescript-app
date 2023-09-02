5b0c88aefd506eeaa6c7e6d61cb1587f679e1cec
dorayx <open@doray.dev>
2023-09-03 03:25:55 +0800

    chore(eslint): add naming conventions for ts exports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:ts-export-naming-convention/recommended',
+    'prettier',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   rules: {},
